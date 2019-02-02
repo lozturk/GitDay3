964a2b396f61213dd5c27fb4103845eb33a9a8ea
lozturk <lozturkk987@Gmail.com>
2019-02-01 22:19:43 -0500

    first commit, feature branch

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -34,14 +34,14 @@ public class LoginTests {
     }
 
     @Test
-    public void logout(){
+    public void loginOut() {
 
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
         Assert.assertEquals(driver.getTitle(), "Web Orders");
-
     }
+
 
 
 
